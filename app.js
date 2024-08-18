e7a5414d4a5572c5477d1e36267bee0a7c53cabc
raulsync <duffer.rdx@gmail.com>
2024-08-18 22:27:40 +0530

    episode03

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-// console.log(global);
+var a = 'namaste Backend';
+
+var b = 10;
+
+var c = 20;
+
+console.log(a);
+console.log(b);
+
+// console.log(global); // it is part of node environment
+
+// console.log(this); // it prints empty object
 
 // console.log(window);  this is not present in node environment
 
-console.log(globalThis);
+console.log(globalThis); // it is standard global object that supports browser, node-environment,web-workers
+
+console.log(globalThis === global);
