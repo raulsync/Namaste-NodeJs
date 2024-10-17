eb3d5f41f07bd735a9d88b0e2d92bb0d0ca9e824
raulsync <rahul.aanand086@gmail.com>
2024-10-17 14:48:52 +0530

    set_timeout_zero_done

--- setTimeoutZero.js
+++ setTimeoutZero.js
@@ -10,10 +10,12 @@ function multiply(a, b) {
 var c = multiply(a, b);
 console.log("Multiplication is : ", c);
 
+//this callback will only push into v8 engine call stack only when call stack is empty
+
 setTimeout(() => {
   console.log("Inside setTimeout Zero");
 }, 0);
 
-// setTimeout(() => {
-//   console.log("call me after 3 seconds");
-// }, 3000);
+setTimeout(() => {
+  console.log("call me after 3 seconds");
+}, 3000);
